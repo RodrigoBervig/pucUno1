bdd002aaf03638498d4da690831ec334996f7567
RodrigoBervig <rodrigo_bervig@hotmail.com>
2018-06-14 22:29:17 -0300

    22:29

--- Uno/Uno/src/Deck.java
+++ Uno/Uno/src/Deck.java
@@ -14,41 +14,25 @@ public class Deck {
 			 */
             deck = new ArrayList<>();
 			preenche(deck);
-			/**String[] cores = {"vermelho","azul","verde","amarelo"};
-			int[] nums = {1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,0}; //cartas normais
-			int[] specialnums = {2,2,4,4}; //Cartas especiais +2, +2, +4 e +4
-			
-            for(String c:cores){ //adicionando cartas normais ao deck
-				for(int i:nums){ //igual a for(int i = 0; i<nums.length; i++)
-					deck.add(new Card(i,c)); //adicionando novas cartas ao deck
-				}
-			}
-
-			for(int i:specialnums){ //Adicionando cartas especiais ao deck
-				deck.add(new Card(i));
-			} */
 		}
-
-		public void preenche(ArrayList<Card> deck){
-		    String[] cores = {"vermelho", "azul", "verde", "amarelo"};
-		    int[] numeros = {1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,0};
-		    int[] numeroEspeciais = {2,2,4,4};
-
-		    for(String cor: cores){ //Igual a for(int i = 0; i<cores.length; i++)
-		        for(int numero: numeros){
-		            deck.add(new Card(numero, cor));
-                }
-            }
-            for(int especial: numeroEspeciais){
-		        deck.add(new Card(especial));
-            }
-        }
-
-
-		
 		public Deck(ArrayList<Card> c){ //construtor overloaded
 			/*Em caso de o deck atual ficar vazio, todas as cartas jogadas são coletadas e se tornam o novo deck;*/
 			deck = c;
+		}
+
+        public void preenche(ArrayList<Card> deck){
+            String[] cores = {"vermelho", "azul", "verde", "amarelo"};
+            int[] numeros = {1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8,9,9,0};
+            int[] numeroEspeciais = {2,2,4,4};
+
+            for(String cor: cores){ //Igual a for(int i = 0; i<cores.length; i++)
+                for(int numero: numeros){
+                    deck.add(new Card(numero, cor));
+                }
+         }
+            for(int especial: numeroEspeciais){
+                deck.add(new Card(especial));
+            }
 		}
 
 		/** Checa o tamanho do deck, se for maior que zero, retorna false, se não, retorna true.*/
@@ -82,14 +66,4 @@ public class Deck {
 			return sb.toString();
 			
 		}
-		
-		
-		
-			
-		
-		
-		
-		
-		
-		
 }
--- Uno/Uno/src/Player.java
+++ Uno/Uno/src/Player.java
@@ -1,136 +1,92 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
-
 public class Player {
-
-	
-	
-	private ArrayList<Card> playercards; 
+    private ArrayList<Card> playercards;
 	private String nome; //nome do jogador
-	
+
+    /**cria um array list que armazena as cartas do jogador, atribui nome ao jogador**/
 	public Player(String nome) {
-		/*
-		 * cria um array list que armazena as cartas do jogador		
-		 * atribui nome ao jogador 
-		 */
-		
-		//objeto jogador é criado na classe Uno
-		
-		playercards = new ArrayList<Card>();
-		this.nome = nome;
-		
+		playercards = new ArrayList<>(); //objeto jogador é criado na classe Uno
+        this.nome = "Convidado";
+        setNome(nome);
 	}
-	
-	public int numberOfCards() {
-		/*
-		 * retorna o numero de cartas que o jogador possui na mão
-		 */
+
+	public void setNome(String nome){
+	    if(!(nome == null)) this.nome = nome;
+    }
+    /**retorna o numero de cartas que o jogador possui na mão**/
+
+    public int numberOfCards(){
 		return playercards.size();
 	}
-	
+
+	/**Retorna todas as cartas que o jogador possui na mão como um ArrayList
+     * Isso é usado para chegar se o jogador tem alguma carta valida para jogar
+     **/
 	public ArrayList<Card> PlayerCards(){
-		/*
-		 * Retorna todas as cartas que o jogador possui na mão como um ArrayList		 
-		 * Isso é usado para chegar se o jogador tem alguma carta valida para jogar		 
-		 */
-		
 		return playercards;
 	}
-	
-	
+
 	public void pickCards(Card c) {
-		/*
-		 * 
-		 */
-		playercards.add(c);
-		
+	    playercards.add(c);
 	}
-	
-	public Card throwCard(int c) {
-		/*
-		 * O jogador joga uma carta de sua mão que esta na posição 'c'. c é um valor integer e é passado como parametro		 
-		 */
-		
-		
+	/** O jogador joga uma carta de sua mão que esta na posição 'c'. c é um valor integer e é passado como parametro**/
+    public Card throwCard(int c) {
 		return playercards.remove(c);
 	}
-	
+
+    /** jogador diz Uno quando tiver apenas uma carta na mão */
 	public void sayUno() {
-		/*
-		 * jogador diz Uno quando tiver apenas uma carta na mão 
-		 */
-		Scanner s = new Scanner(System.in);
-		
-		if (playercards.size()==1){
-			
-			System.out.println("Uno");
+        Scanner s = new Scanner(System.in);
+        if (playercards.size()==1){
+            System.out.println("Uno");
 			System.out.println("Pressione Enter...");
 			s.nextLine();
 		}
 	}
-	
-	
-	public void showCards() {
-		/*
-		 * Isso é uma representação gráfica de uma carta		 
-		 * apenas para fazer as cartas ficarem um pouco mais parecidas com cartas hahaha		 
-		 * usado no método showboard() na classe Uno 
-		 */
-		
+    /** Isso é uma representação gráfica de uma carta
+       apenas para fazer as cartas ficarem um pouco mais parecidas com cartas hahaha
+       usado no método showboard() na classe Uno
+     **/
+    public void showCards() {
+		StringBuilder sb = new StringBuilder();
 		String[] card = {" ----- ","|     |","|     |"," ----- "};
 		String c = "";
-		
-		
-		for(int i=0;i<card.length;i++) {
-				
-			for(int j=0;j<playercards.size();j++) {
-					
-				if(!playercards.get(j).isSpecial()) {
-					if(i==1) {
-						
-						c = c +"| "+playercards.get(j).getColor()+" |"+" ";
-						
+    for(int i=0;i<card.length;i++) {
+        for(int j=0;j<playercards.size();j++){
+            if(!playercards.get(j).isSpecial()) {
+                if(i==1) {
+                        sb.append("| ").append(playercards.get(j).getColor()).append(" |").append(" ");
+					    //c = c +"| "+playercards.get(j).getColor()+" |"+" ";
 					}
-					
 					else if(i==2) {
-						
-						c = c + "|  "+playercards.get(j).getValue()+"  |"+" ";
-					}
-					
-					else {
-						c = c + card[i]+" "; 
-					}
-					
-					}
-				
-				else if(playercards.get(j).isSpecial()) {
-					
-					if(i==1) {
-						
-						c = c +"| "+"+"+playercards.get(j).getValue()+"  |"+" ";
-						
+                    sb.append("| ").append(playercards.get(j).getValue()).append(" |").append(" ");
+					    //c = c + "|  "+playercards.get(j).getValue()+"  |"+" ";
 					}
 					else {
-						c = c + card[i]+" ";
+                        sb.append(card[i]).append(" ");
+						//c = c + card[i]+" ";
 					}
-					
-				}
-				
+            }
+            else if(playercards.get(j).isSpecial()) {
+                if(i==1) {
+                    sb.append("| ").append(playercards.get(j).getValue()).append(" |").append(" ");
+                    //c = c +"| "+"+"+playercards.get(j).getValue()+"  |"+" ";
+                }
+					else {
+                        sb.append(card[i]).append(" ");
+						//c = c + card[i]+" ";
 					}
-				
-				c +="\n";
-				
-			}
-				
-		
-		
-		
-		System.out.print(c);
-	}
+            }
+        }
+        sb.append("\n");
+        //c +="\n";
+        }
+        System.out.print(sb.toString());
+    }
 	
 	public void hideCards() {
-		
+        StringBuilder sb = new StringBuilder();
 		/*
 		 * esconde cartas do jogador		 
 		 * usado no método showboard() na classe Uno 
@@ -142,10 +98,9 @@ public class Player {
 		
 		for(int i=0;i<card.length;i++) {
 				
-			for(int j=0;j<playercards.size();j++) {
-		
+			for(int j=0;j<playercards.size();j++){
+			    sb.append(c).append(card[i]);
 				c = c + card[i]+" ";
-		
 			}
 		c+="\n";
 		}
